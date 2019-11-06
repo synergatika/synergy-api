14eceabe40aad51a9c634ebd788e37290a33e649
George Theofilis <theofilis.g@gmail.com>
2019-11-06 14:14:00 +0200

    Integrate contract in tests

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env sh
 
+sudo apt install g++-7
+
 docker run -d -p 27017:27017 mongo
+
+export CXX="g++-7"
+
+git clone https://github.com/synergatika/loyalty-contracts.git contracts
+
+cd contracts
+
+npm install -g ganache-cli truffle
+npm install
+
+ganache-cli -l 100000000 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" > /dev/null &
+TESTRPC_PID=$!
+trap "kill $TESTRPC_PID" EXIT INT TERM
+
+truffle compile
+truffle migrate
+
+cd ..
 
 npm install
 npx webpack
