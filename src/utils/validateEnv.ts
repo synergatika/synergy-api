23d4e4f335d847e84328c36e633cb9c9d091ca4c
George Theofilis <theofilis.g@gmail.com>
2019-12-12 23:24:02 +0200

    Check all enviroment options validity

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,12 +1,28 @@
 import {
-  cleanEnv, str, port
+  cleanEnv, str, port, host, num, email, url
 } from 'envalid';
- 
+
 export default function validateEnv() {
   cleanEnv(process.env, {
-    DB_PASSWORD: str(),
+    API_URL: url(),
+    APP_URL: url(),
+    DB_HOST: host(),
     DB_NAME: str(),
-    DB_USER: str(),
+    DB_PASSWORD: str(),
     DB_PORT: port(),
+    DB_USER: str(),
+    EMAIL_FROM: str({ default: 'Synergy | A Social and Solidarity Economy Toolkit' }),
+    EMAIL_HOST: host(),
+    EMAIL_PASS: str(),
+    EMAIL_PORT: port({ default: 587}),
+    EMAIL_USER: email(),
+    ETH_API_ACCOUNT_PRIVKEY: str(),
+    ETH_CONTRACTS_PATH: str(),
+    ETH_REMOTE_API: host({ default: 'localhost' }),
+    JWT_EXPIRATION: num(),
+    JWT_SECRET: str(),
+    PORT: port({ default: 3000 }),
+    TOKEN_EXPIRATION: num({ default: 5 }),
+    TOKEN_LENGTH: num({ default: 32 }),
   });
 }
