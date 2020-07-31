19a1cf619bc8e14aaac83ae0437528ff6c8da182
Dimitris Mytakis <dmytakis@gmail.com>
2020-07-31 15:33:18 +0300

    translation file

---
+++ src/translation.ts
@@ -0,0 +1,106 @@
+class Translation {
+
+  public content = {
+    ["en-EN" as string]: {
+      communication: {
+        "subject": "You have an Email from a User",
+        "title": "A user needs to communicate with our team.",
+        "body": "",
+        "button": ""
+      },
+      invitation: {
+        "subject": "You have been invited to Synergy Community",
+        "title": "You 've got an invitation",
+        "body": "",
+        "button": ""
+      },
+      deactivation: {
+        "subject": "Synergy Account deactivated",
+        "title": "A users ask to deactivate his/her account",
+        "body": "",
+        "button": ""
+      },
+      reactivation: {
+        "subject": "Synergy Account is Active again",
+        "title": "You account is active again",
+        "body": "",
+        "button": ""
+      },
+      registration: {
+        "subject": "Welcome to Synergy Team",
+        "title": "Login and update your password",
+        "body": "",
+        "button": ""
+      },
+      restoration: {
+        "subject": "Update your Password",
+        "title": "Follow the link to update your password",
+        "body": "",
+        "button": ""
+      },
+      verification: {
+        "subject": "You need to verify your account",
+        "title": "Follow the link to validate your email address",
+        "body": "",
+        "button": ""
+      },
+      common: {
+        "IF_LINK": "If that doesn't work, copy and paste the following link in your browser:",
+        "CHEERS": "Cheers",
+        "TEAM": "Synergy Team",
+        "FOOTER": "You received this email because we received a request for your account. If you didn't request you can safely delete this email."
+      }
+    },
+    ["el-EL" as string]: {
+      communication: {
+        "subject": "Επικοινωνία από Χρήστη",
+        "title": "Ένας χρήστης προσπαθεί να επικοινωνήσει με την Ομάδα",
+        "body": "",
+        "button": ""
+      },
+      invitation: {
+        "subject": "Έχεις πρόσκληση για την Κοινότητα του Synergy",
+        "title": "Μόλις έλαβες μια πρόσκληση",
+        "body": "",
+        "button": ""
+      },
+      deactivation: {
+        "subject": "Ο Λογαριασμοός σας απενεργοποιήθηκε",
+        "title": "Ένας Χρήστης θέλει να απενεργοποιήσει το λογαρισμό του/της",
+        "body": "",
+        "button": ""
+      },
+      reactivation: {
+        "subject": "Ο Λογαρισμός σας είναι Ενεγός και πάλι",
+        "title": "Καλώς ήρθες πίσω",
+        "body": "",
+        "button": ""
+      },
+      registration: {
+        "subject": "Καλώς ήρθατε στην κοινότητα του Synergy",
+        "title": "Κάντε σύνδεση και ανανεώστε τον προσωρινό Κωδικό Πρόσβασης",
+        "body": "",
+        "button": ""
+      },
+      restoration: {
+        "subject": "Ανανέωση Κωδικού Πρόσβασης",
+        "title": "Ακολουθήστε το παρακάτω λινκ",
+        "body": "",
+        "button": ""
+      },
+      verification: {
+        "subject": "Επιβεβαίωση Διεύθυνσης Ηλεκτρονικού Ταχυδρομείου",
+        "title": "Ακολουθήστε το παρακάτω λινκ",
+        "body": "",
+        "button": ""
+      },
+      common: {
+        "IF_LINK": "Εαν δεν μεταφερθείτε αυτόματα στη σελίδα μας, κάντε αντιγραφή/επικόλληση τον παρακάτω συνδεσμο",
+        "CHEERS": "Χαιρετισμούς",
+        "TEAM": "η Ομάδα του Synergy",
+        "FOOTER": "Λάβατε αυτό το μήνυμα ηλεκτρονικού ταχυδρομίου, καθώς στάλθηκε μια αίτηση από το συγκεκριμένο λογαριασμό. Έαν, η αίτηση δεν στάλθηκε από εσάς, μπορείται να διαγράψετε με ασφάλεια αυτο το μήνυμα."
+      }
+    }
+  }
+}
+export default Translation;
--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -20,6 +20,7 @@ import UnprocessableEntityException from '../exceptions/UnprocessableEntity.exce
 // Interfaces
 import RequestWithUser from '../interfaces/requestWithUser.interface';
 
+import Translation from '../translation';
 class EmailService {
 
 
@@ -32,91 +33,8 @@ class EmailService {
   // Email: To member (when redeem an order or part of an order).
 
 
-  private content = {
-    ["en-EN" as string]: {
-      communication: {
-        "subject": "You have an Email fror a User",
-        "title": "",
-        "body": ""
-      },
-      invitation: {
-        "subject": "You have been invited to Synergy Community",
-        "title": "",
-        "body": ""
-      },
-      deactivation: {
-        "subject": "Synergy Account deactivated",
-        "title": "",
-        "body": ""
-      },
-      reactivation: {
-        "subject": "Synergy Account is Active again",
-        "title": "",
-        "body": ""
-      },
-      registration: {
-        "subject": "Welcome to Synergy Team",
-        "title": "",
-        "body": ""
-      },
-      restoration: {
-        "subject": "Update your Password",
-        "title": "",
-        "body": ""
-      },
-      verification: {
-        "subject": "You need to verify your account",
-        "title": "",
-        "body": ""
-      },
-      common: {
-        "IF_LINK": "If that doesn't work, copy and paste the following link in your browser:",
-        "CHEERS": "Cheers,<br>Synergy Team",
-        "FOOTER": "You received this email because we received a request for your account. If you didn't request you can safely delete this email."
-      }
-    },
-    ["el-EL" as string]: {
-      communication: {
-        "subject": "Επικοινωνία από Χρήστη",
-        "title": "",
-        "body": ""
-      },
-      invitation: {
-        "subject": "Έχεις πρόσκληση για την Κοινότητα του Synergy",
-        "title": "",
-        "body": ""
-      },
-      deactivation: {
-        "subject": "Ο Λογαριασμοός σας απενεργοποιήθηκε",
-        "title": "",
-        "body": ""
-      },
-      reactivation: {
-        "subject": "Ο Λογαρισμός σας είναι Ενεγός και πάλι",
-        "title": "",
-        "body": ""
-      },
-      registration: {
-        "subject": "Καλώς ήρθατε στην κοινότητα του Synergy",
-        "title": "",
-        "body": ""
-      },
-      restoration: {
-        "subject": "Ανανέωση Κωδικού Πρόσβασης",
-        "title": "",
-        "body": ""
-      },
-      verification: {
-        "subject": "Επιβεβαίωση Διεύθυνσης Ηλεκτρονικού Ταχυδρομείου",
-        "title": "",
-        "body": ""
-      },
-      common: {
-        "IF_LINK": "Εαν δεν μεταφερθείτε αυτόματα στη σελίδα μας, κάντε αντιγραφή/επικόλληση τον παρακάτω συνδεσμο",
-        "CHEERS": "Χαιρετισμούς,<br>η Ομάδα του Synergy",
-        "FOOTER": "Λάβατε αυτό το μήνυμα ηλεκτρονικού ταχυδρομίου, καθώς στάλθηκε μια αίτηση από το συγκεκριμένο λογαριασμό. Έαν, η αίτηση δεν στάλθηκε από εσάς, μπορείται να διαγράψετε με ασφάλεια αυτο το μήνυμα."
-      }
-    }
+  private translation = (lang: string) => {
+    return (new Translation).content[lang];
   }
 
   private emailSender = (options: { type: any; locals: any; from: any; to: any; subject: any; }) => {
@@ -146,20 +64,18 @@ class EmailService {
     let options = {
       from: process.env.EMAIL_FROM,
       to: data.user.email,
-      subject: this.content[lang].verification.subject,//'Email Verification',
+      subject: this.translation(lang).verification.subject,//'Email Verification',
       html: '',
       type: 'verification',
       locals: {
-        ...this.content[lang].common,
-        ...this.content[lang].verification,
+        ...this.translation(lang).common,
+        ...this.translation(lang).verification,
         logo_url: `https://app.synergatika.gr/assets/media/images/logo.png`,
         home_page: `${process.env.APP_URL}`,
         link: `${process.env.APP_URL}auth/verify-email/${data.token}`
       },
     }
 
-    console.log("subject", this.content["el-EL"].verification.subject);
-    console.log("options", options.locals)
     let error, results: object = {};
     [error, results] = await to(this.emailSender(options));
     if (error) return next(new UnprocessableEntityException(`EMAIL ERROR || ${error}`));
@@ -174,12 +90,12 @@ class EmailService {
     let options = {
       from: process.env.EMAIL_FROM,
       to: data.user.email,
-      subject: this.content[lang].restoration.subject,//'Password Restoration',
+      subject: this.translation(lang).restoration.subject,//'Password Restoration',
       html: '',
       type: 'restoration',
       locals: {
-        ...this.content[lang].common,
-        ...this.content[lang].restoration,
+        ...this.translation(lang).common,
+        ...this.translation(lang).restoration,
         logo_url: `https://app.synergatika.gr/assets/media/images/logo.png`,
         home_page: `${process.env.APP_URL}`,
         link: `${process.env.APP_URL}auth/reset-password/${data.token}`
@@ -201,12 +117,12 @@ class EmailService {
       let options = {
         from: process.env.EMAIL_FROM,
         to: data.user.email,
-        subject: this.content[lang].registration.subject,//'New Account',
+        subject: this.translation(lang).registration.subject,//'New Account',
         html: '',
         type: 'registration',
         locals: {
-          ...this.content[lang].common,
-          ...this.content[lang].registration,
+          ...this.translation(lang).common,
+          ...this.translation(lang).registration,
           logo_url: `https://app.synergatika.gr/assets/media/images/logo.png`,
           home_page: `${process.env.APP_URL}`,
           link: `${process.env.APP_URL}auth/login/`,
@@ -228,12 +144,12 @@ class EmailService {
     let options = {
       from: process.env.EMAIL_FROM,
       to: data.user.email,
-      subject: this.content[lang].reactivation.subject,//'Account Activation',
+      subject: this.translation(lang).reactivation.subject,//'Account Activation',
       html: '',
       type: 'reactivation',
       locals: {
-        ...this.content[lang].common,
-        ...this.content[lang].reactivation,
+        ...this.translation(lang).common,
+        ...this.translation(lang).reactivation,
         logo_url: `https://app.synergatika.gr/assets/media/images/logo.png`,
         home_page: `${process.env.APP_URL}`,
         link: `${process.env.APP_URL}auth/login/`
@@ -254,12 +170,12 @@ class EmailService {
     let options = {
       from: process.env.EMAIL_FROM,
       to: process.env.EMAIL_FROM,
-      subject: this.content[lang].deactivation.subject,//'Account Deactivation',
+      subject: this.translation(lang).deactivation.subject,//'Account Deactivation',
       html: '',
       type: 'deactivation',
       locals: {
-        ...this.content[lang].common,
-        ...this.content[lang].deactivation,
+        ...this.translation(lang).common,
+        ...this.translation(lang).deactivation,
         logo_url: `https://app.synergatika.gr/assets/media/images/logo.png`,
         home_page: `${process.env.APP_URL}`,
         email: data.user.email,
@@ -281,12 +197,12 @@ class EmailService {
     let options = {
       from: process.env.EMAIL_FROM,
       to: data.receiver,
-      subject: this.content[lang].invitation.subject,//'User Invitation',
+      subject: this.translation(lang).invitation.subject,//'User Invitation',
       html: '',
       type: 'invitation',// `invitation-${lang.toString()}`,
       locals: {
-        ...this.content[lang].common,
-        ...this.content[lang].invitation,
+        ...this.translation(lang).common,
+        ...this.translation(lang).invitation,
         logo_url: `https://app.synergatika.gr/assets/media/images/logo.png`,
         home_page: `${process.env.APP_URL}`,
         email: data.user.email
@@ -307,12 +223,12 @@ class EmailService {
     let options = {
       from: process.env.EMAIL_FROM,
       to: process.env.EMAIL_FROM,
-      subject: this.content[lang].communication.subject,//'User Communication',
+      subject: this.translation(lang).communication.subject,//'User Communication',
       html: '',
       type: 'communication',
       locals: {
-        ...this.content[lang].common,
-        ...this.content[lang].communication,
+        ...this.translation(lang).common,
+        ...this.translation(lang).communication,
         logo_url: `https://app.synergatika.gr/assets/media/images/logo.png`,
         home_page: `${process.env.APP_URL}`,
         sender: data.sender,
